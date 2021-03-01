49f4c9e150fca488f5c821da5baa8bce840375b9
Gustav Lau <gustavlau@gmail.com>
2021-03-01 11:35:34 -0500

    Small layout change and added multiple choice option

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 let playerGen = document.querySelector('.player-gen');
 let playerGenPicture = document.querySelector('.player-gen-picture');
+let correctPlayerName = "";
+let choice1 = document.getElementById('choice-1');
+let choice2 = document.getElementById('choice-2');
+let choice3 = document.getElementById('choice-3');
+let choice4 = document.getElementById('choice-4');
 
-
-
-async function getPlayerHeight(){
+async function generatePlayer(){
     // event.preventDefault(); 
     // const name = document.querySelector('input[name="name"]').value;
     while(playerGenPicture.firstChild){ 
@@ -11,17 +14,45 @@ async function getPlayerHeight(){
     }
     const response = await fetch ("https://data.nba.net/data/10s/prod/v1/2020/players.json");
     const data = await response.json();
-    console.log(data.league.standard);
-    let randomPlayer=data.league.standard[Math.floor(Math.random()*data.league.standard.length)];
-    console.log(randomPlayer);
+    let fullPlayerArr=data.league.standard    
+    shufflePlayersArr(fullPlayerArr);
+    console.log(shufflePlayersArr(fullPlayerArr));
+    let fourChoice = [fullPlayerArr[0],fullPlayerArr[1], fullPlayerArr[2], fullPlayerArr[3]];
+    console.log(fourChoice);
+
+    choice1.innerText = fourChoice[0].firstName;
+    choice2.innerText = fourChoice[1].firstName;
+    choice3.innerText = fourChoice[2].firstName;
+    choice4.innerText = fourChoice[3].firstName;
+    let randomPlayer=fourChoice[Math.floor(Math.random()*fourChoice.length)];
+    console.log(randomPlayer);    
     let playerId=randomPlayer.personId
-    console.log(playerId);
+    // console.log(playerId);
     const image = document.createElement('img');
     const url = `https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/${playerId}.png`
     image.setAttribute('src', url);
     playerGenPicture.appendChild(image)
     playerGen.innerText= randomPlayer.lastName;
-
+    correctPlayerName= randomPlayer.firstName;
+    return correctPlayerName;
 }
 
-let  generateButton = document.getElementById('generate-player').addEventListener('click', getPlayerHeight)
+function shufflePlayersArr(arr){
+    let j, x, index;
+    for (index = arr.length -1; index >0; index--){
+        j = Math.floor(Math.random()*(index+1));
+        x = arr[index];
+        arr[index] = arr[j];
+        arr[j]=x;
+    }
+    return arr;
+}
+
+function correctPlayer(player, selection){
+    if (player === selection){
+        console.log("Correct!");
+        generatePlayer();
+    }
+}
+
+let  generateButton = document.getElementById('generate-player').addEventListener('click', generatePlayer)
