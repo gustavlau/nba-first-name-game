02544d9e2180c48d28c94b13f9ae2e3c8f1264e6
Gustav Lau <gustavlau@gmail.com>
2021-04-27 23:49:28 -0400

    timer function add - incomplete

--- script.js
+++ script.js
@@ -65,26 +65,40 @@ generatePlayer();
 // Timer
 // ###################
 let timer = document.getElementById("timer");
-let min = 0;
-let sec = 0;
-let msec = 0;
+let timeDiff, min, sec, msec;
 let stopTime = false;
 
 function startTimer(){
     if (stopTime){
-        console.log("I am now running");
-        min = parseInt(min);
-        sec = parseInt(sec);
-        msec = parseInt(msec);
+        let getTime = Date.now();
+        timeDiff = 60000;
 
-        msec = msec +1;
+        console.log(getTime);
 
-        timer.innerHTML = min + ':' + sec + ':' + msec;
-
-        // setTimeout("startTimer()", 10);
     }
     
 }
+
+// function timeToString(time) {
+//     let diffInHrs = time / 3600000;
+//     let hh = Math.floor(diffInHrs);
+  
+//     let diffInMin = (diffInHrs - hh) * 60;
+//     let mm = Math.floor(diffInMin);
+  
+//     let diffInSec = (diffInMin - mm) * 60;
+//     let ss = Math.floor(diffInSec);
+  
+//     let diffInMs = (diffInSec - ss) * 100;
+//     let ms = Math.floor(diffInMs);
+  
+//     let formattedMM = mm.toString().padStart(2, "0");
+//     let formattedSS = ss.toString().padStart(2, "0");
+//     let formattedMS = ms.toString().padStart(2, "0");
+  
+//     return `${formattedMM}:${formattedSS}:${formattedMS}`;
+//   }
+  
 
 
 // ###################
